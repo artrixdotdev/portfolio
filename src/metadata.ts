b2e0a32c373a2a22984453c05688bc18e83e7ee4
Artrix9095 <sefu786@outlook.com>
2023-12-03 04:58:02 +0000

    Initial commit

---
+++ src/metadata.ts
@@ -0,0 +1,11 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  description: "Artrix's Portfolio",
+};
+
+export const socials = {
+  DISCORD_ID: "361628174842593280",
+  DISCORD_TAG: "artrix_",
+  GITHUB_TAG: "Artrix9095",
+};

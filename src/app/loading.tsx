637d4586f3fd24b0b87ca27f0535ffaf427e9b47
Artrix9095 <sefu786@outlook.com>
2025-01-23 09:43:01 -0800

    Add a loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { LogoOutline } from "@/components/icons";
+import { motion } from "framer-motion";
+
+export default function Loading() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 100 }}
+      transition={{ duration: 0.4, ease: "easeInOut" }}
+      className="flex h-screen pb-32  items-center justify-center"
+    >
+      <LogoOutline className="h-48 w-48 animate-hue-rotate" />
+    </motion.div>
+  );
+}

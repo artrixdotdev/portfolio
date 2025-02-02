f7698b666c431ceef525c90b35f3857c29e06de4
Artrix9095 <sefu786@outlook.com>
2025-02-01 19:38:22 -0800

    Remove unneeded variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,6 @@ import {
 import { GradiantBackground } from "@/components/radial-gradient";
 
 export default function Home() {
-  const objects = [
-    7,
-    Math.random() * Math.random() * (Math.random() > 0.5 ? 7 : 2),
-  ]
-    .map(Math.round)
-    .map((m) => (m < 2 ? 1 : m))
-    .map((m) => ({ mass: m, volume: m }));
-
   return (
     <>
       <GradiantBackground />

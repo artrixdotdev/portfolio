c81b21e3d13791cd71797277fadf31204333327e
Artrix9095 <sefu786@outlook.com>
2025-02-11 10:22:14 -0800

    Increase margin top

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -6,7 +6,7 @@ export default function BlogLayout({
    children: React.ReactNode;
 }) {
    return (
-      <div className="w-full flex flex-col mt-24 items-center justify-center">
+      <div className="w-full flex flex-col mt-36 items-center justify-center">
          <div className="grid w-[800px] max-w-[80%] gap-8 mb-32">
             {children}
          </div>
